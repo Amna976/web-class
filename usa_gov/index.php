87f6eb8d885dc7a6709fceb798a82f25246b784b
Amna976 <99017457+Amna976@users.noreply.github.com>
2022-02-04 15:43:11 +0500

    Add files via upload

---
+++ usa_gov/index.php
@@ -0,0 +1,25 @@
+	<?php include 'template/header.php' ?>
+
+	<nav class="navbar navbar-expand-lg nav1">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<img src="asserts/favicon-57.png" alt="U.S. flag" class="img1">&nbsp;
+					<span class="p1">An official website of the United States government</span> 
+					<a class="a1 btn btn-link" data-toggle="collapse" data-target="#collapse" role="button">Here's how you know &nbsp;<b><i class="fa fa-angle-down" aria-hidden="true" class="fa fa-cog" style="font-size: 18px;"></i></b></a>
+					<div class="collapse" id="collapse">
+						<div class="container">
+							<div class="row">
+							<div class="col-lg-5">
+								<img src="asserts/svg1.JPG">
+							</div>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+
+<?php include 'template/footer.php' ?>
